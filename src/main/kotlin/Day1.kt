276afbabc61dff03f8ac675552af60180774d108
Gergely István Mess <messgeri@edu.bme.hu>
2023-12-07 00:57:00 +0100

    day1 half implemented

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -7,4 +7,23 @@ class Day1 : IAdventOfCode {
         return 1
     }
 
+    fun ParseLine(input : String) : Long{
+        var first = 0L
+        var last = 0L
+        var foundFirst = false
+
+        input.forEach {
+            if(it.isDigit()){
+                if(foundFirst){
+                    last = it.digitToInt().toLong()
+                }else{
+                    foundFirst = true
+                    first = it.digitToInt().toLong()
+                    last = it.digitToInt().toLong()
+                }
+            }
+        }
+        return first * 10 + last
+    }
+
 }
