9cf1910277b01749476077e35f5c80abf6281043
Gergely István Mess <messgeri@edu.bme.hu>
2023-12-07 21:55:18 +0100

    I hate number 9 but 2 star

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,5 +1,5 @@
 class Day1 : IAdventOfCode {
-    private var solution = 0L
+    private var solution = 0
     override val day = 1
 
     override fun solve(input : String) {
@@ -7,31 +7,164 @@ class Day1 : IAdventOfCode {
         print("The solution is: $solution")
     }
 
-    fun parseLine(input : String) : Long{
-        var first = 0L
-        var last = 0L
+    fun parseLine(input : String) : Int{
+        var first = 0
+        var last = 0
         var foundFirst = false
+        var number = 0
+        val states = arrayListOf(0,0,0,0,0,0,0,0,0)
 
         input.forEach {
-            if(it.isDigit()){
+            for(i in 0..8){
+                states[i] = getState(it, states[i], i)
+            }
+
+            if(states[0] == 3) number = 1
+            if(states[1] == 3) number = 2
+            if(states[2] == 5) number = 3
+            if(states[3] == 4) number = 4
+            if(states[4] == 4) number = 5
+            if(states[5] == 3) number = 6
+            if(states[6] == 5) number = 7
+            if(states[7] == 5) number = 8
+            if(states[8] == 4) number = 9
+            if(it.isDigit() && it.digitToInt() > 0) number = it.digitToInt()
+
+
+            //Save numbers
+            if(number > 0){
                 if(foundFirst){
-                    last = it.digitToInt().toLong()
+                    last = number
                 }else{
                     foundFirst = true
-                    first = it.digitToInt().toLong()
-                    last = it.digitToInt().toLong()
+                    last = number
+                    first = number
                 }
             }
         }
         return first * 10 + last
     }
 
-    fun parseText(input : String) : Long{
-        var retVal = 0L
+    fun parseText(input : String) : Int{
+        var retVal = 0
 
         input.lines().forEach {
-            retVal += parseLine(it)
+            val num = parseLine(it)
+            retVal += num
+            println("$it value: $num / $retVal")
         }
         return retVal
+    }
+
+    private fun getState(c : Char, v : Int, state : Int) : Int{
+        when(state){
+            0 -> return state1(c,v)
+            1 -> return state2(c,v)
+            2 -> return state3(c,v)
+            3 -> return state4(c,v)
+            4 -> return state5(c,v)
+            5 -> return state6(c,v)
+            6 -> return state7(c,v)
+            7 -> return state8(c,v)
+            8 -> return state9(c,v)
+        }
+        return 0
+    }
+
+    private fun state1(c : Char, v : Int) : Int{
+        when(c){
+            'o' -> return 1
+            'n' -> if(v == 1) return 2
+            'e' -> if(v == 2) return 3
+        }
+        return 0
+    }
+
+    private fun state2(c : Char, v : Int) : Int{
+        when(c){
+            't' -> return 1
+            'w' -> if(v == 1) return 2
+            'o' -> if(v == 2) return 3
+        }
+        return 0
+    }
+
+    private fun state3(c : Char, v : Int) : Int{
+        when(c){
+            't' -> return 1
+            'h' -> if(v == 1) return 2
+            'r' -> if(v == 2) return 3
+            'e' -> if(v == 3) {
+                return 4
+            }else if(v == 4){
+                return 5
+            }
+        }
+        return 0
+    }
+
+    private fun state4(c : Char, v : Int) : Int{
+        when(c){
+            'f' -> return 1
+            'o' -> if(v == 1) return 2
+            'u' -> if(v == 2) return 3
+            'r' -> if(v == 3) return 4
+        }
+        return 0
+    }
+
+    private fun state5(c : Char, v : Int) : Int{
+        when(c){
+            'f' -> return 1
+            'i' -> if(v == 1) return 2
+            'v' -> if(v == 2) return 3
+            'e' -> if(v == 3) return 4
+        }
+        return 0
+    }
+
+    private fun state6(c : Char, v : Int) : Int{
+        when(c){
+            's' -> return 1
+            'i' -> if(v == 1) return 2
+            'x' -> if(v == 2) return 3
+        }
+        return 0
+    }
+
+    private fun state7(c : Char, v : Int) : Int{
+        when(c){
+            's' -> return 1
+            'v' -> if(v == 2) return 3
+            'n' -> if(v == 4) return 5
+            'e' -> if(v == 1) {
+                return 2
+            }else if(v == 3){
+                return 4
+            }
+        }
+        return 0
+    }
+
+    private fun state8(c : Char, v : Int) : Int{
+        when(c){
+            'e' -> return 1
+            'i' -> if(v == 1) return 2
+            'g' -> if(v == 2) return 3
+            'h' -> if(v == 3) return 4
+            't' -> if(v == 4) return 5
+        }
+        return 0
+    }
+
+    private fun state9(c : Char, v : Int) : Int{
+        when(c){
+            'n' -> if(v == 2){
+                return 3
+            }else return 1
+            'i' -> if(v == 1) return 2
+            'e' -> if(v == 3) return 4
+        }
+        return 0
     }
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -7,7 +7,7 @@ internal class Day1Test{
     @Test
     fun testParseLine(){
         val examples = arrayOf("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet")
-        var results = ArrayList<Long>(4 )
+        var results = ArrayList<Int>(4 )
 
         examples.forEach {
             results.add(day.parseLine(it))
@@ -29,5 +29,54 @@ internal class Day1Test{
         val result = day.parseText(example)
 
         assertEquals(result, 142)
+    }
+
+    @Test
+    fun testParseWrittenNumbers(){
+        val examples = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        var results = ArrayList<Int>(9)
+
+        examples.forEach {
+            results.add(day.parseLine(it))
+        }
+
+        for (i in 0..8){
+            assertEquals((i + 1)*10 + i+1, results[i])
+        }
+    }
+
+    @Test
+    fun testParseTextWithWrittenNumbers(){
+        val example = "two1nine\n" +
+                "eightwothree\n" +
+                "abcone2threexyz\n" +
+                "xtwone3four\n" +
+                "4nineeightseven2\n" +
+                "zoneight234\n" +
+                "7pqrstsixteen"
+
+        val result = day.parseText(example)
+
+        assertEquals(result, 281)
+    }
+
+    @Test
+    fun testStartWithEndCases(){
+        val examples = arrayOf("nineight", "threeight", "fiveight", "oneight", "twone", "eighthree", "eightwo", "sevenine", "threight")
+        var results = arrayListOf<Int>()
+
+        examples.forEach {
+            results.add(day.parseLine(it))
+        }
+
+        assertEquals(98, results[0])
+        assertEquals(38, results[1])
+        assertEquals(58, results[2])
+        assertEquals(18, results[3])
+        assertEquals(21, results[4])
+        assertEquals(83, results[5])
+        assertEquals(82, results[6])
+        assertEquals(79, results[7])
+        assertEquals(88, results[8])
     }
 }
