6f2e15fdb73c49221022ff4b2908fc3e05fb7bf0
Gergely István Mess <messgeri@edu.bme.hu>
2023-12-09 02:58:57 +0100

    day4 2-star

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,34 +1,60 @@
+import kotlin.math.log2
+
 class Day4 : IAdventOfCode {
     override val day = 4
     override var mode = SolutionMode.OneStar
+    private var cards = ArrayList<Int>()
 
     private var s1 = 0L
     var solutionOneStar : Long
         get() = s1
         private set(value) { s1 = value }
 
+    private var s2 = 0L
+    var solutionTwoStar : Long
+        get() = s2
+        private set(value) { s2 = value }
+
     override fun solve(input: String) {
+        clear()
+        for(j in input.lines().indices) cards.add(1)
+        var i = 0
         input.lines().forEach{
-            solutionOneStar += getWinning(it)
+            val copies = getWinning(it)
+            var prize = 0
+            for(n in 1..copies){
+                if(n == 1)
+                    prize += 1
+                else
+                    prize *= 2
+            }
+
+            solutionOneStar += prize
+            solutionTwoStar += cards[i]
+            i++
+            for(j in 0 until copies){
+                if(j + i > cards.size) break
+                cards[j + i] += cards[i-1]
+            }
         }
-        println("The solution for 1 star is: $solutionOneStar")
+        println("The solution for 1 star is: $solutionTwoStar")
     }
 
     private fun getWinning(card : String) : Int {
         var prize = 0
         var c = card.split(':')
         c = c[1].split('|')
-        var w = c[0].split(' ')
-        var n = c[1].split(' ')
+        val w = c[0].split(' ')
+        val n = c[1].split(' ')
 
-        var winners = ArrayList<Int>()
-        var numbers = ArrayList<Int>()
+        val winners = ArrayList<Int>()
+        val numbers = ArrayList<Int>()
 
         w.forEach{
-            it.toIntOrNull()?.let { it -> winners.add(it) }
+            it.toIntOrNull()?.let { it1 -> winners.add(it1) }
         }
         n.forEach {
-            it.toIntOrNull()?.let { it -> numbers.add(it) }
+            it.toIntOrNull()?.let { it1 -> numbers.add(it1) }
         }
 
         winners.sort()
@@ -37,15 +63,16 @@ class Day4 : IAdventOfCode {
         winners.forEach{
             while(numbers.isNotEmpty() && i < numbers.size && numbers[i] <= it){
                 if(numbers[i] == it){
-                    if(prize == 0){
-                        prize = 1
-                    }else{
-                        prize *= 2
-                    }
+                    prize += 1
                 }
                 i++
             }
         }
         return prize
+    }
+
+    fun clear(){
+        solutionOneStar = 0L
+        solutionTwoStar = 0L
     }
 }
